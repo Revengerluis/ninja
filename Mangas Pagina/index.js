501937ed6e58b9dd2dfe84addea5a68679812bb6
Lavaca03 <156031903+Lavaca03@users.noreply.github.com>
2024-05-09 17:38:14 -0400

    Add files via upload

---
+++ Mangas Pagina/index.js
@@ -0,0 +1,282 @@
+function Buscar() {
+   var input = document.getElementById("nombrepelicula").value;
+
+   // Redireccionar a la página de resultados con el término de búsqueda como parámetro
+   window.location.href = "Resultados.html?q=" + encodeURIComponent(input);
+}
+
+// Función para redirigir a otra página HTML
+function Mangas() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Mangas.html';
+}
+
+// Función para redirigir a otra página HTML
+function Manhwas() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Manhwas.html';
+}
+
+// Función para redirigir a otra página HTML
+function Inicio() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'index.html';
+}
+
+// Función para redirigir a otra página HTML
+function bokunohero() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Bokunohero.html';
+}
+
+// Función para redirigir a otra página HTML
+function jujutsukaisen() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Jujutsukaisen.html';
+}
+
+
+// Función para redirigir a otra página HTML
+function kaijun8() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Kaijun8.html';
+}
+
+// Función para redirigir a otra página HTML
+function sakamotodays() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Sakamotodays.html';
+}
+
+// Función para redirigir a otra página HTML
+function frieren() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Frieren.html';
+}
+
+// Función para redirigir a otra página HTML
+function familiayozakura() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Familiayozakura.html';
+}
+
+// Función para redirigir a otra página HTML
+function spyxfamily() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Spyxfamily.html';
+}
+
+// Función para redirigir a otra página HTML
+function kimetsunoyaiba() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Kimetsunoyaiba.html';
+}
+
+// Función para redirigir a otra página HTML
+function gachiakuta() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Gachiakuta.html';
+}
+
+// Función para redirigir a otra página HTML
+function fairytail100() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Fairytail100.html';
+}
+
+// Función para redirigir a otra página HTML
+function bluelock() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Bluelock.html';
+}
+
+// Función para redirigir a otra página HTML
+function shuumatsu() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Shuumatsu.html';
+}
+
+// Función para redirigir a otra página HTML
+function borutotbv() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Borutotbv.html';
+}
+
+// Función para redirigir a otra página HTML
+function dragonball() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Dragonballsuper.html';
+}
+
+// Función para redirigir a otra página HTML
+function onepiece() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Onepiece.html';
+}
+
+// Función para redirigir a otra página HTML
+function dandadan() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Dandadan.html';
+}
+function iruma() {
+   window.location.href = 'Mairimashita.html';
+}
+
+// Función para redirigir a otra página HTML
+function kagurabachi() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Kagurabachi.html';
+}
+
+// Función para redirigir a otra página HTML
+function saintseiya() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Saintseiyanext.html';
+}
+
+// Función para redirigir a otra página HTML
+function edens() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Edenszero.html';
+}
+
+// Función para redirigir a otra página HTML
+function kingdom() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Kingdom.html';
+}
+
+
+// Función para redirigir a otra página HTML
+function vinland() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Vinlandsaga.html';
+}
+
+// Función para redirigir a otra página HTML
+function ragna() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Ragnacrimson.html';
+}
+
+// Función para redirigir a otra página HTML
+function ron() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Kamonahashi.html';
+}
+
+// Función para redirigir a otra página HTML
+function wind() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'windbreaker.html';
+}
+
+// Función para redirigir a otra página HTML
+function servamp() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'Servamp.html';
+}
+
+// Función para redirigir a otra página HTML
+function undead() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'undeadunluck.html';
+}
+
+// Función para redirigir a otra página HTML
+function one() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'onepunchman.html';
+}
+
+// Función para redirigir a otra página HTML
+function Black() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'blackclover.html';
+}
+
+// Función para redirigir a otra página HTML
+function berserk() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'berserk.html';
+}
+
+// Función para redirigir a otra página HTML
+function radiant() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'radiant.html';
+}
+
+// Función para redirigir a otra página HTML
+function trigger() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'worldtrigger.html';
+}
+
+// Función para redirigir a otra página HTML
+function shangri() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'shangri.html';
+}
+
+// Función para redirigir a otra página HTML
+function monster() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'remonster.html';
+}
+
+// Función para redirigir a otra página HTML
+function goblin() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'goblin.html';
+}
+
+// Función para redirigir a otra página HTML
+function beyblade() {
+   // Cambia 'otra_pagina.html' por la URL de la página a la que quieres redirigir
+   window.location.href = 'beybladex.html';
+}
+
+// Función para detectar bloqueadores de anuncios
+document.addEventListener('DOMContentLoaded', function () {
+   var adBlockEnabled = false;
+   var testAd = document.createElement('div');
+   testAd.innerHTML = '&nbsp;';
+   testAd.className = 'adBanner';
+   document.body.appendChild(testAd);
+   window.setTimeout(function () {
+      if (testAd.offsetHeight === 0) {
+         adBlockEnabled = true;
+      }
+      testAd.remove();
+      if (adBlockEnabled) {
+         console.log('¡Bro, Quita el bloqueador de Anuncios para continuar!');
+         // Aquí podrías realizar alguna acción, como mostrar un mensaje al usuario.
+      } else {
+         console.log('No se detectó un bloqueador de anuncios.');
+      }
+   }, 100);
+});
+
+
+function Pagina1() {
+   window.location.href = 'Mangas.html'; // Reemplaza 'index.html' con la URL de tu página principal
+}
+
+function Pagina2() {
+   window.location.href = 'Mangas2.html'; // Reemplaza 'pagina_dos.html' con la URL de tu segunda página
+}
+
+function Pagina3() {
+   window.location.href = 'Mangas3.html'; // Reemplaza 'pagina_dos.html' con la URL de tu segunda página
+}
+
+
+(function () { // DON'T EDIT BELOW THIS LINE
+   var d = document, s = d.createElement('script');
+   s.src = 'https://https-www-animehokage-com.disqus.com/embed.js';
+   s.setAttribute('data-timestamp', +new Date());
+   (d.head || d.body).appendChild(s);
+})();
+
